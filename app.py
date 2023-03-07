d08eb846b4a1b3278d0c2321e7c19de87017e5bc
Omar Anwar <113179786+omaranwarfpt@users.noreply.github.com>
2023-03-07 16:24:37 +0500

    Create app.py

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
+import time
+
+import redis
 from flask import Flask
-from datetime import datetime
-import re
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
